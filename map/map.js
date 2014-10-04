c2824ae6a02712d3328a40168cd47cbc4a8eb13d
Michael Xu <michaeljxu11@gmail.com>
2014-10-04 12:36:32 -0700

    helper functions + listener

--- map/map.js
+++ map/map.js
@@ -26,6 +26,44 @@ function getWaypoints(tripId, callback) {
   });
 }
 
+fb.child('trip1').on('value', function(data) {
+  var waypoints = data.val();
+  if (!waypoints) {
+    callback("Error obtaining waypoints! Does this trip exist?", "");
+    return;
+  }
+  // Ideally you would call calcRoute(waypoints);
+  calcRoute();
+});
+
+function addWaypointToTrip(tripId, waypointNumber, waypointObj) {
+  fb.child(tripId).child('waypoints').once('value', function(data) {
+    var waypoints = data.val();
+    if (waypoints) {
+      waypoints['waypoint' + waypointNumber] = waypointObj;
+      fb.child(tripId).child('waypoints').set(waypoints);
+    }
+  });
+}
+
+function appendWaypoint(tripId, waypointObj) {
+  fb.child(tripId).child('waypoints').once('value', function(data) {
+    var waypoints = data.val();
+    if (waypoints) {
+      var num = Object.keys(waypoints).length + 1;
+      addWaypointToTrip(tripId, num, waypointObj);
+    }
+  });
+}
+
+function addTrip(tripId, waypointsList) {
+  fb.child(tripId).child('waypoints').set(waypointsList);
+}
+
+addTrip('trip2', {'waypoint1': {'location':{'lat': 24.222, 'lng': 27.211}, 'name': 'supdawg', 'stopover':true}});
+appendWaypoint('trip2', {'location':{'lat': 17, 'long':42}, 'name':'yo', 'stopover':true});
+
+
 getWaypoints('trip1', function(err, data) {
   console.log(data);
 });
