a557cbb19343af8a5ed2f208a88b8af1b5fdc5b1
kevinychen <binonunquineist@gmail.com>
2014-10-04 21:13:32 -0400

    Add text to speech.

--- android/src/com/example/waypal/MainActivity.java
+++ android/src/com/example/waypal/MainActivity.java
@@ -1,17 +1,33 @@
 package com.example.waypal;
 
+import java.util.Locale;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
+
+    TextToSpeech ttobj;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ttobj = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR){
+                    ttobj.setLanguage(Locale.UK);
+                }
+            }
+        });
     }
 
 
@@ -32,5 +48,12 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void speakText(View view) {
+        String toSpeak = "I can speak!";
+        Toast.makeText(getApplicationContext(), toSpeak,
+        Toast.LENGTH_SHORT).show();
+        ttobj.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
     }
 }
