7e66041b99a9329d38025bc85cb54b6e9aff5160
ytung <ytung@mit.edu>
2014-10-04 22:41:03 -0400

    added listview

--- android/src/com/example/waypal/DisplayMessageActivity.java
+++ android/src/com/example/waypal/DisplayMessageActivity.java
@@ -21,6 +21,48 @@ public class DisplayMessageActivity extends Activity {
 			getFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
+		// Get ListView object from xml
+        listView = (ListView) findViewById(R.id.list);
+        
+        // Defined Array values to show in ListView
+        String[] values = new String[] { "Android List View", 
+                                         "Adapter implementation",
+                                         "Simple List View In Android",
+                                         "Create List View Android", 
+                                         "Android Example", 
+                                         "List View Source Code", 
+                                         "List View Array Adapter", 
+                                         "Android Example List View" 
+                                        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+          android.R.layout.activity_display_message, android.R.id.list, values);
+
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter); 
+        
+        // ListView Item Click Listener
+        listView.setOnItemClickListener(new OnItemClickListener() {
+
+              @Override
+              public void onItemClick(AdapterView<?> parent, View view,
+                 int position, long id) {
+                
+               // ListView Clicked item index
+               int itemPosition     = position;
+               
+               // ListView Clicked item value
+               String  itemValue    = (String) listView.getItemAtPosition(position);
+                  
+                // Show Alert 
+                Toast.makeText(getApplicationContext(),
+                  "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
+                  .show();
+             
+              }
+
+         }); 
 	}
 
 	@Override
